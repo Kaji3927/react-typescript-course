ab63858cd8b8a474671d4eb76e9b6e75de9f746f
Kaji <k.kaji3927@gmail.com>
2023-11-19 20:41:03 +0900

    Section3-No.38

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
 import CourseGoal from "./components/CourseGoal";
 import Header from "./components/Header";
 import goalImage from "./assets/goals.jpg";
+import { useState } from "react";
+import CourseGoalList from "./components/CourseGoalList";
+
+export type CourseGoal = {
+  title: string;
+  description: string;
+  id: number;
+};
 
 export default function App() {
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
+  const handleAddGoal = () => {
+    setGoals((prevGoals) => {
+      const newGoal: CourseGoal = {
+        id: Math.random(),
+        title: "Learn React + TS",
+        description: "Learn it in depth!",
+      };
+      return [...prevGoals, newGoal];
+    });
+  };
   return (
     <main>
       <Header image={{ src: goalImage, alt: "A list of goals" }}>
         <h1>Your Course Goals</h1>
       </Header>
-      <CourseGoal title="Learn React + TS">
-        <p>Learn it from the ground up</p>
-      </CourseGoal>
+      <button onClick={handleAddGoal}>Add Goal</button>
+      <CourseGoalList goals={goals} />
     </main>
   );
 }
